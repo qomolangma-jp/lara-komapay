1ed9661ad4ad3b0bdb489a7b0e47d78da79ce185
Bungoyoshiba <contact@qomolangma.jp>
2026-06-02 00:40:24 +0900

    0602-01

--- config/services.php
+++ config/services.php
@@ -5,4 +5,13 @@ return [
         'channel_access_token' => env('LINE_CHANNEL_ACCESS_TOKEN', ''),
         'channel_secret' => env('LINE_CHANNEL_SECRET', ''),
     ],
+
+    'paypay' => [
+        'api_key'      => env('PAYPAY_API_KEY', ''),
+        'api_secret'   => env('PAYPAY_API_SECRET', ''),
+        'merchant_id'  => env('PAYPAY_MERCHANT_ID', ''),
+        'api_base_url' => env('PAYPAY_API_BASE_URL', 'https://api.paypay.ne.jp'),
+        'redirect_url' => env('PAYPAY_REDIRECT_URL', ''),
+        'sandbox'      => env('PAYPAY_SANDBOX', true),
+    ],
 ];
